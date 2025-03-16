58d5f7362b57772dcfb3e9a8d7bf752466f04626
Sofiia <sofiamyugrieva47@gmail.com>
2025-03-16 22:40:27 +0300

    Лекция 5, дз

---
+++ home_work/5_hw.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+field_user_name = driver.find_element(By.CSS_SELECTOR, '#user-name')
+field_password = driver.find_element(By.CSS_SELECTOR, '#password')
+button_submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
+if field_user_name and field_password and button_submit is None:
+    print('Элементы не найдены')
+else:
+    print('Элементы найдены')
